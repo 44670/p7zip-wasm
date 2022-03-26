04fa2993139c720ff93929c4f73117bb0a12b9ac
44670 <44670@users.noreply.github.com>
2022-03-26 21:09:52 +0800

    workable (?)

--- wasm-port/wasm.cpp
+++ wasm-port/wasm.cpp
@@ -60,13 +60,131 @@
 #include "7zip/MyVersion.h"
 #endif
 
-CCodecs *codecs = new CCodecs;
-CMyComPtr<IUnknown> __codecsRef = codecs;
-CArc *currentArc = NULL;
 extern "C"
 {
 
+    uint32_t apiExtractOneFileTarget = 0;
+
+    CCodecs *codecs = new CCodecs;
+    CMyComPtr<IUnknown> __codecsRef = codecs;
+
+    class MyOutStream : public ISequentialOutStream
+    {
+    public:
+        int idx;
+        STDMETHOD(Write)(const void *data, UInt32 size, UInt32 *processedSize) {
+            // Call wasmOnWrite on the JS side
+            EM_ASM({
+                wasmOnWrite($0, $1, $2);
+            }, idx, data, size);
+            *processedSize = size;
+            return S_OK;
+        }
+        STDMETHOD(QueryInterface)
+        (REFGUID iid, void **outObject)
+        {
+            return E_NOINTERFACE;
+        }
+        STDMETHOD_(ULONG, AddRef)
+        ()
+        {
+            return 1;
+        }
+        STDMETHOD_(ULONG, Release)
+        ()
+        {
+            return 1;
+        }
+    };
+
+    class MyCompressProgressInfo : public ICompressProgressInfo
+    {
+        STDMETHOD(SetRatioInfo)
+        (const UInt64 *inSize, const UInt64 *outSize)
+        {
+            return S_OK;
+        }
+        STDMETHOD(QueryInterface)
+        (REFIID iid, void **outObject)
+        {
+            return E_NOINTERFACE;
+        }
+        STDMETHOD_(ULONG, AddRef)
+        () { return 1; }
+        STDMETHOD_(ULONG, Release)
+        () { return 1; }
+    };
+    MyCompressProgressInfo g_myCompressProgressInfo;
+    class MyExtractCallback : public IArchiveExtractCallback
+    {
+        STDMETHOD(SetTotal)
+        (UInt64 size)
+        {
+            return S_OK;
+        }
+        STDMETHOD(SetCompleted)
+        (const UInt64 *completeValue)
+        {
+            return S_OK;
+        }
+        STDMETHOD(GetStream)
+        (UInt32 index, ISequentialOutStream **outStream, Int32 askExtractMode)
+        {
+            if (index != apiExtractOneFileTarget) {
+                printf("Skipping idx: %d\n", index);
+                return S_OK;
+            }
+            auto s = new MyOutStream();
+            s->idx = index;
+            *outStream = s;
+            return S_OK;
+        }
+        STDMETHOD(PrepareOperation)
+        (Int32 askExtractMode)
+        {
+            return S_OK;
+        }
+        STDMETHOD(SetOperationResult)
+        (Int32 resultEOperationResult)
+        {
+            return S_OK;
+        }
+        STDMETHOD(QueryInterface)
+        (REFGUID iid, void **outObject)
+        {
+            int groupID = iid.Data4[3];
+            int subID = iid.Data4[5];
+            printf("QueryInterface: %d %d\n", groupID, subID);
+            if (groupID == 4)
+            {
+                if (subID == 4)
+                {
+                    *outObject = &g_myCompressProgressInfo;
+                    return S_OK;
+                }
+            }
+            return E_NOTIMPL;
+        }
+        STDMETHOD_(ULONG, AddRef)
+        ()
+        {
+            return 1;
+        }
+        STDMETHOD_(ULONG, Release)
+        ()
+        {
+            return 1;
+        }
+    };
+
+    MyExtractCallback myExtractCallback;
+
+#define API_GENERAL_ERROR (-1000)
+#define API_NO_MEMORY (-1001)
+
     static wchar_t listBuf[1 * 1024 * 1024];
+    CArc *currentArc;
+
     void *getSymbol(int id)
     {
         if (id == 0)
@@ -91,13 +209,13 @@ extern "C"
     {
         CArchiveLink *al = new CArchiveLink();
         COpenOptions op;
-        op.filePath = L"archive";
+        op.filePath = L"/worker/archive";
         op.codecs = codecs;
         HRESULT ret = al->Open(op);
         printf("OpenArchive len: %d\n", al->Arcs.Size());
         if (al->Arcs.Size() <= 0)
         {
-            return -1000;
+            return API_GENERAL_ERROR;
         }
         currentArc = &(al->Arcs.Back());
         return (int)ret;
@@ -109,7 +227,7 @@ extern "C"
         int bufPos = 0;
         if (currentArc == NULL)
         {
-            return -1000;
+            return API_GENERAL_ERROR;
         }
         // Iterate over all files in the archive
         UInt32 itemCount = 0;
@@ -117,18 +235,44 @@ extern "C"
         printf("ItemCount: %d\n", itemCount);
         for (int i = 0; i < itemCount; i++)
         {
-            UString result;
-            currentArc->GetItemPath2(i, result);
-            int len = (int)result.Len();
-            if (bufPos + len + 1 > sizeof(listBuf))
+            CReadArcItem item;
+            currentArc->GetItem(i, item);
+            bool defined = false;
+            uint64_t size = 0;
+            currentArc->GetItemSize(i, size, defined);
+            FILETIME time = {0};
+            currentArc->GetItemMTime(i, time, defined);
+            size_t dataLen = 7 + item.Path.Len() + 1;
+            if (bufPos + dataLen > sizeof(listBuf))
             {
-                return -1001;
+                return API_NO_MEMORY;
             }
-            memcpy(listBuf + bufPos, result.Ptr(), len * sizeof(wchar_t));
-            listBuf[bufPos + len] = 0xCAFEBABE;
-            bufPos += len + 1;
+            listBuf[bufPos] = 0xCAFEBABE;
+            listBuf[bufPos + 1] = item.IsDir ? 1 : 0;
+            listBuf[bufPos + 2] = (uint32_t)size;
+            listBuf[bufPos + 3] = (uint32_t)(size >> 32);
+            listBuf[bufPos + 4] = time.dwLowDateTime;
+            listBuf[bufPos + 5] = time.dwHighDateTime;
+            listBuf[bufPos + 6] = item.Path.Len();
+            bufPos += 7;
+            memcpy(listBuf + bufPos, item.Path.Ptr(), item.Path.Len() * sizeof(wchar_t));
+            bufPos += item.Path.Len();
         }
         return bufPos;
+    }
+
+    int apiExtractOneFile(uint32_t idx)
+    {
+        apiExtractOneFileTarget = idx;
+        if (currentArc == NULL)
+        {
+            return NULL;
+        }
+        CMyComPtr<IInArchive> archive = currentArc->Archive;
+        uint32_t indicies[1] = {(uint32_t)idx};
+
+        archive->Extract(indicies, 1, 0, &myExtractCallback);
+        return 0;
     }
 
     int main(int argc, char **argv)
---
+++ wasm-port/worker.js
@@ -0,0 +1,170 @@
+// Worker for processing archive files
+var state = {}
+var wasmOutFileMap = {}
+
+function wasmReady() {
+    // Send message to main page
+    self.postMessage({
+        t: 'ready'
+    });
+}
+
+// Handle messages from main page
+self.onmessage = function (e) {
+    var data = e.data
+    var type = data.t;
+    var cmdMap = {
+        'open': openArchvie,
+        'extract': extractOneFile,
+    }
+    var fn = cmdMap[type];
+    if (!fn) {
+        console.log('Unknown command', type);
+        return;
+    }
+    fn(data);
+}
+
+
+// Load the WASM module (build/7za.js)
+importScripts('7za.js');
+
+
+
+function WasmOutFile() {
+    this.bufs = [];
+    this.len = 0;
+    this.write = function (u8Arr) {
+        var tmpBuf = new Uint8Array(u8Arr.length);
+        tmpBuf.set(u8Arr);
+        this.bufs.push(tmpBuf);
+    }
+}
+
+function wasmOnWrite(idx, ptr, len) {
+    console.log('wasmOnWrite', idx, ptr, len);
+    var f = wasmOutFileMap[idx]
+    if (!f) {
+        console.log('skip');
+        return;
+    }
+    f.write(Module.HEAPU8.subarray(ptr, ptr + len));
+}
+
+
+function openArchvie(data) {
+    var blob = data.blob
+    blob.name = 'archive';
+    console.log('worker openArchvie', blob);
+    // Mount workerfs
+    FS.mkdir('/worker');
+    FS.mount(WORKERFS, {
+        blobs: [{
+            name: 'archive',
+            data: blob
+        }]
+    }, '/worker');
+    var ret = Module._apiOpenArchive();
+    if (ret != 0) {
+        self.postMessage({
+            t: 'error',
+            msg: 'Failed to open archive',
+            ret: ret
+        });
+        return;
+    }
+    var bufLen = Module._apiListFiles();
+    console.log('wasmListBuf Len:', bufLen);
+    var wasmListBuf = Module.HEAPU32.subarray(Module._getSymbol(0) / 4, 1 * 1024 * 1024);
+    var fileList = []
+    var str = ''
+    var pos = 0;
+    var idx = 0
+    while (pos < bufLen) {
+        if (wasmListBuf[pos] != 0xCAFEBABE) {
+            console.log("Invalid magic number at " + pos);
+            break;
+        }
+        var filetype = wasmListBuf[pos + 1];
+        var fileSize = (wasmListBuf[pos + 3] << 32) | (wasmListBuf[pos + 2]);
+        var fileTime = wasmListBuf[pos + 5];
+        var strLen = wasmListBuf[pos + 6];
+        var str = ''
+        for (var i = 0; i < strLen; i++) {
+            str += String.fromCharCode(wasmListBuf[pos + 7 + i]);
+        }
+        fileList.push({
+            type: filetype,
+            size: fileSize,
+            time: fileTime,
+            name: str,
+            idx: idx
+        })
+        idx += 1
+        pos += 7 + strLen;
+    }
+
+    console.log(fileList)
+    state = {
+        mode: 'open',
+        fileList: fileList,
+        currentDir: [],
+        root: fileListToDirectory(fileList)
+    }
+    self.postMessage({
+        t: 'opened',
+        state: state
+    });
+}
+
+
+
+function fileListToDirectory(lst) {
+    var ret = {}
+    for (var i = 0; i < lst.length; i++) {
+        var fobj = lst[i]
+        var arr = fobj.name.split('/')
+        var isDir = fobj.type & 1
+        var dir = ret
+        for (var j = 0; j < arr.length - 1; j++) {
+            var dirName = arr[j]
+            if (!dir[dirName]) {
+                dir[dirName] = { type: 1, files: {} }
+            }
+            dir = dir[dirName].files
+        }
+        if (isDir) {
+            dir[arr[arr.length - 1]] = { type: 1, files: {} }
+        } else {
+            dir[arr[arr.length - 1]] = fobj
+        }
+    }
+    return { type: 1, files: ret }
+}
+
+
+
+function extractOneFile(data) {
+    var idx = data.idx
+    var fout = new WasmOutFile();
+    var name = data.name
+
+    wasmOutFileMap[idx] = fout;
+    var ret = Module._apiExtractOneFile(idx)
+    if (ret != 0) {
+        alert('Extract failed ' + ret);
+        return;
+    }
+    wasmOutFileMap[idx] = null;
+
+    var blob = new Blob(fout.bufs, {
+        type: 'application/octet-stream'
+    });
+    // Post message to main page
+    self.postMessage({
+        t: 'extracted',
+        idx: idx,
+        blob: blob,
+        name: name
+    });
+}
